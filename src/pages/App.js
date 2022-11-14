601c2eea076bcc13540ca6993c2ffea1801f7298
Bamarcheti <bmarchetifiorin@gmail.com>
2022-11-14 08:43:43 -0400

    movendo App para dentro da pasta pages

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import '../pages/App.css';
+import Search from './Search/Search';
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
