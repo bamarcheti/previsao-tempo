677b90511b11b6425030cad91ba70d422bced4c2
Bamarcheti <bmarchetifiorin@gmail.com>
2022-11-14 22:31:57 -0400

    excluindo importação desnecessária

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,10 @@
 import '../pages/App.css';
-// import GetClima from './Search/components/GetClima';
 import Search from './Search/Search';
 
 const App = () => {
   return (
     <div className="App">
       <Search />
-      {/* <GetClima /> */}
     </div>
   );
 }
