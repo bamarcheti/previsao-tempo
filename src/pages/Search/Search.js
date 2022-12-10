ed1ed9991f79efb854e05da16cf9e891a4f3a556
Bamarcheti <bmarchetifiorin@gmail.com>
2022-12-10 17:39:17 -0400

    componentização api

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,45 +1,17 @@
 import { React, useState } from 'react';
+import Api from '../../components/Api';
 import './Search.css';
 
 const Search = () => {
-  const keyID = '276028a84e6f633afbd7b4e1d68552bf';
-
   const [valorPesquisa, setValorPesquisa] = useState("");
   const [cidade, setCidade] = useState("");
-
-  const resultadoAtualizado = () => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${valorPesquisa}&appid=${keyID}&units=metric`)
-      .then(res => res.json())
-      .then(data => {
-        const { main, name, sys, weather } = data;
-        if (sys !== undefined) {
-          if (weather !== undefined) {
-            setCidade(
-              `<div className="resultado row">
-                <div>
-                  <h1>${Math.round(main.temp)}°</h1>
-                  <h4>${sys.country}</h4>
-                  <h4>${name}</h4>
-                  <h4>${weather[0]['description']}</h4>
-                </div>
-                <div>
-                  <h4>🌡️</h4>
-                  <h4>Máx<span>↑</span>${Math.round(main.temp_max)}°</h4>
-                  <h4>Mín<span>↓</span>${Math.round(main.temp_min)}°</h4>
-                </div>
-              </div>`
-            );
-          }
-        }
-      });
-  }
 
   return (
     <div className="searchWraper">
       <div className="search">
         <h3>Digite o nome da cidade:</h3>
         <input placeholder={'Digite aqui...'} onChange={(evt) => { setValorPesquisa(evt.target.value) }} type="text" />
-        <button onClick={() => resultadoAtualizado()}>Pesquisar</button>
+        <button onClick={() => Api(valorPesquisa)}>Pesquisar</button>
       </div>
 
       <br />
