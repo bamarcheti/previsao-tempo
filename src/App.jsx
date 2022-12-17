e741d7d2a8731efb12256da381e439a59210ad3c
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2022-12-17 17:22:15 -0400

    globalizando rotas

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppRoutes from './routes/App.routes';
+
+const App = () => {
+  return (
+  <AppRoutes />
+)
+};
+
+export default App;
---
+++ src/routes/App.routes.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import MainLayout from '../components/MainLayout/MainLayout';
+import Random from '../pages/Random';
+import Search from '../pages/Search';
+
+
+const AppRoutes = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<MainLayout />}>
+          <Route index element={<Search />} />
+          <Route path='previsao-aleatoria' element={<Random />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default AppRoutes;
